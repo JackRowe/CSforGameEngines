8dfd749a7430b380019179e6ef1cb2b49e798af0
Jack Rowe <jack.rowe@outlook.com>
2024-02-19 00:11:23 +0000

    compass for velocity

--- Assets/Scripts/Interface.cs
+++ Assets/Scripts/Interface.cs
@@ -7,15 +7,26 @@ using UnityEngine.UI;
 public class InterfaceController : MonoBehaviour
 {
     GameObject player;
+    Rigidbody2D rb;
+
+    TextMeshProUGUI velocityText;
+    RectTransform velocityTransform;
 
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        rb = player.GetComponent<Rigidbody2D>();
+        velocityText = transform.Find("Compass/VelocityText").GetComponent<TextMeshProUGUI>();
+        velocityTransform = transform.Find("Compass/Velocity").GetComponent<RectTransform>();
     }
 
     private void Update()
     {
-        if (player == null) { return; }
-        
+        if (player == null || rb == null) { return; }
+        velocityText.SetText($"{Mathf.Round(rb.velocity.magnitude * 2)} KM/h");
+        Quaternion velocityRotation = Quaternion.LookRotation(rb.velocity, -Vector3.forward); ;
+        velocityTransform.rotation = new Quaternion(0,0,-velocityRotation.y,velocityRotation.w);
+        velocityTransform.sizeDelta = new Vector2(3, Mathf.Clamp(rb.velocity.magnitude * 2, 0, 40));
+
     }
 }
