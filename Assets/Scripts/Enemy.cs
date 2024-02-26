ed2c785abefb41afef6a6c1d72c3846bee40b4e6
Jack Rowe <jack.rowe@outlook.com>
2024-02-26 14:14:03 +0000

    enemy sprite & basic rotation

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float speed = 2.0f;
+    [SerializeField] float rotSpeed = 4.0f;
+    GameObject target;
+    Rigidbody2D rb;
+    Rigidbody2D rbTarget;
+
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        rbTarget = target.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!rb) return;
+
+        Vector3 targetDistance = target.transform.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(targetDistance + (new Vector3(rbTarget.velocity.x, rbTarget.velocity.y) * (targetDistance.magnitude / 2)), -Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, new Quaternion(0, 0, -targetRotation.y, targetRotation.w), rotSpeed / 100);
+    }
+}
